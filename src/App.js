62f55a22b75a2e881afd256a85da4a311556e87c
John Lee <john.lee@deliveryhero.com>
2022-02-16 10:55:51 +0100

    add shop page, collection preview, and collection item

--- src/App.js
+++ src/App.js
@@ -1,46 +1,16 @@
 import React from 'react';
-import { Switch, Route, Link } from 'react-router-dom'
+import { Switch, Route } from 'react-router-dom'
 import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component'
 
-const HatsPage= () => {
-  return (
-    <div>
-    {/* <button onClick={() => props.history.push('/topics2')}>Topics2</button>
-      <Link to='/topics'>Topics</Link> */}
-      <h1>HATS PAGE</h1>
-    </div>
-  )
-}
-// const TopicsList= (props) => {
-//   return (
-//     <div>
-//       <h1>TOPIC LIST PAGE</h1>
-//       <Link to={`${props.match.url}/13`}>TO TOPIC 13 </Link>
-//       <Link to={`${props.match.url}/17`}>TO TOPIC 17 </Link>
-//       <Link to={`${props.match.url}/21`}>TO TOPIC 21 </Link>
-//     </div>
-//   )
-// }
-// const TopicsDetail= (props) => {
-//   console.log(props)
-//   return (
-//     <div>
-//       <h1>TOPIC DETAIL PAGE: {props.match.params.topicId}</h1>
-//     </div>
-//   )
-// }
 function App() {
   return (
     <div>
       <Switch>
         <Route exact path="/" component={HomePage} />
-        <Route path='/hats' component={HatsPage} />
-        {/* <Route exact path="/whateverthepathishere/topics" component={TopicsList} />
-        <Route path="/whateverthepathishere/topics/:topicId" component={TopicsDetail} />
-        <Route exact path="/blog/topics" component={TopicsList} />
-        <Route path="/blog/topics/:topicId" component={TopicsDetail} /> */}
+        <Route path="/shop" component={ShopPage} />
       </Switch>
     </div>
   )
--- src/index.js
+++ src/index.js
@@ -7,9 +7,11 @@ import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>,
   document.getElementById('root')
 );
 
