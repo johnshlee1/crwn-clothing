694e618cbfc74e77406eb84efaf16980bc95b973
John Lee <john.lee@deliveryhero.com>
2022-02-18 22:22:38 +0100

    add new state cartItem to store in an array, and increase the quantity of the existing item instead of adding as a new object in the array

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,40 +1,42 @@
-import React from 'react'
-import { connect } from 'react-redux'
-import './header.styles.scss'
-import { Link } from 'react-router-dom'
-import { auth } from '../../firebase/firebase.utils'
-import CartIcon from '../cart-icon/cart-icon.component'
-import CartDropdown from '../cart-dropdown/cart-dropdown.component'
-import {ReactComponent as Logo} from '../../assets/crown.svg'
+import React from "react";
+import { connect } from "react-redux";
+import "./header.styles.scss";
+import { Link } from "react-router-dom";
+import { auth } from "../../firebase/firebase.utils";
+import CartIcon from "../cart-icon/cart-icon.component";
+import CartDropdown from "../cart-dropdown/cart-dropdown.component";
+import { ReactComponent as Logo } from "../../assets/crown.svg";
 
 const Header = ({ currentUser, hidden }) => (
-  <div className='header'>
+  <div className="header">
     <Link to="/">
-      <Logo className='logo' />
+      <Logo className="logo" />
     </Link>
-    <div className='options'>
-      <Link className='option' to='/shop'>
+    <div className="options">
+      <Link className="option" to="/shop">
         SHOP
       </Link>
-      <Link className='option' to='/shop'>
+      <Link className="option" to="/shop">
         CONTACT
       </Link>
-      {
-        currentUser ?
-        <div className='option' onClick={() => auth.signOut()}>SIGN OUT</div>
-        : <Link className='option' to='/signin'>
+      {currentUser ? (
+        <div className="option" onClick={() => auth.signOut()}>
+          SIGN OUT
+        </div>
+      ) : (
+        <Link className="option" to="/signin">
           SIGN IN
         </Link>
-      }
+      )}
       <CartIcon />
     </div>
     {hidden ? null : <CartDropdown />}
   </div>
-)
+);
 
-const mapStateToProps = ({user: { currentUser }, cart: { hidden }}) => ({
+const mapStateToProps = ({ user: { currentUser }, cart: { hidden } }) => ({
   currentUser,
-  hidden
-})
+  hidden,
+});
 
-export default connect(mapStateToProps)(Header)
+export default connect(mapStateToProps)(Header);
